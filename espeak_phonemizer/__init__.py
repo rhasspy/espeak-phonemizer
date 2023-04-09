530a74cada5b01f63515ddef686cd0ef6cf8571b
Michael Hansen <mike@rhasspy.org>
2023-04-09 11:30:17 -0500

    Use TextToPhonemes

--- espeak_phonemizer/__init__.py
+++ espeak_phonemizer/__init__.py
@@ -15,7 +15,6 @@ class Phonemizer:
     Use ctypes and libespeak-ng to get IPA phonemes from text.
     Not thread safe.
 
-    Requires libc.so.6
     Tries to use libespeak-ng.so or libespeak-ng.so.1
     """
 
@@ -26,7 +25,6 @@ class Phonemizer:
     AUDIO_OUTPUT_SYNCHRONOUS = 0x02
     espeakPHONEMES_IPA = 0x02
     espeakCHARS_AUTO = 0
-    espeakSSML = 0x10
     espeakPHONEMES = 0x100
 
     LANG_SWITCH_FLAG = re.compile(r"\([^)]*\)")
@@ -44,7 +42,6 @@ class Phonemizer:
         self.default_voice = default_voice
         self.clause_breakers = clause_breakers or Phonemizer.DEFAULT_CLAUSE_BREAKERS
 
-        self.libc: typing.Any = None
         self.lib_espeak: typing.Any = None
 
     def phonemize(
@@ -57,7 +54,6 @@ class Phonemizer:
         punctuation_separator: str = "",
         keep_language_flags: bool = False,
         no_stress: bool = False,
-        ssml: bool = False,
     ) -> str:
         """
         Return IPA string for text.
@@ -72,7 +68,6 @@ class Phonemizer:
             punctuation_separator: Separator string between before punctuation (keep_clause_breakers=True)
             keep_language_flags: True if language switching flags should be kept
             no_stress: True if stress characters should be removed
-            ssml: True if text may contain SSML tags
 
         Returns:
             ipa - string of IPA phonemes
@@ -91,88 +86,69 @@ class Phonemizer:
         if keep_clause_breakers and self.clause_breakers:
             missing_breakers = [c for c in text if c in self.clause_breakers]
 
-        # Create in-memory file for phoneme trace.
-        # espeak_TextToPhonemes segfaults no matter what I do, so this is the back-up.
-        phonemes_buffer = ctypes.c_char_p()
-        phonemes_size = ctypes.c_size_t()
-        phonemes_file = self.libc.open_memstream(
-            ctypes.byref(phonemes_buffer), ctypes.byref(phonemes_size)
-        )
+        phoneme_flags = Phonemizer.espeakPHONEMES_IPA
+        if phoneme_separator:
+            phoneme_flags = phoneme_flags | (ord(phoneme_separator) << 8)
 
-        try:
-            phoneme_flags = Phonemizer.espeakPHONEMES_IPA
-            if phoneme_separator:
-                phoneme_flags = phoneme_flags | (ord(phoneme_separator) << 8)
+        text_bytes = text.encode("utf-8")
+        text_pointer = ctypes.c_char_p(text_bytes)
 
-            self.lib_espeak.espeak_SetPhonemeTrace(phoneme_flags, phonemes_file)
+        text_flags = Phonemizer.espeakCHARS_AUTO
 
-            text_bytes = text.encode("utf-8")
-
-            synth_flags = Phonemizer.espeakCHARS_AUTO | Phonemizer.espeakPHONEMES
-            if ssml:
-                synth_flags |= Phonemizer.espeakSSML
-
-            self.lib_espeak.espeak_Synth(
-                text_bytes,
-                0,  # buflength (unused in AUDIO_OUTPUT_SYNCHRONOUS mode)
-                0,  # position
-                0,  # position_type
-                0,  # end_position (no end position)
-                synth_flags,
-                None,  # unique_speaker,
-                None,  # user_data,
+        phoneme_lines = []
+        while text_pointer:
+            clause_phonemes = ctypes.c_char_p(
+                self.lib_espeak.espeak_TextToPhonemes(
+                    ctypes.pointer(text_pointer), text_flags, phoneme_flags,
+                )
             )
-            self.libc.fflush(phonemes_file)
-
-            phoneme_lines = ctypes.string_at(phonemes_buffer).decode().splitlines()
-
-            if not keep_language_flags:
-                # Remove language switching flags, e.g. (en)
-                phoneme_lines = [
-                    Phonemizer.LANG_SWITCH_FLAG.sub("", line) for line in phoneme_lines
-                ]
-
-            if word_separator != " ":
-                # Split/re-join words
-                for line_idx in range(len(phoneme_lines)):
-                    phoneme_lines[line_idx] = word_separator.join(
-                        phoneme_lines[line_idx].split()
-                    )
-
-            # Re-insert clause breakers
-            if missing_breakers:
-                # pylint: disable=consider-using-enumerate
-                for line_idx in range(len(phoneme_lines)):
-                    if line_idx < len(missing_breakers):
-                        phoneme_lines[line_idx] += (
-                            punctuation_separator + missing_breakers[line_idx]
-                        )
-
-            phonemes_str = word_separator.join(line.strip() for line in phoneme_lines)
-
-            if no_stress:
-                # Remove primary/secondary stress markers
-                phonemes_str = Phonemizer.STRESS_PATTERN.sub("", phonemes_str)
-
-            # Clean up multiple phoneme separators
-            if phoneme_separator:
-                phonemes_str = re.sub(
-                    "[" + re.escape(phoneme_separator) + "]+",
-                    phoneme_separator,
-                    phonemes_str,
+            if clause_phonemes.value is not None:
+                phoneme_lines.append(
+                    clause_phonemes.value.decode()  # pylint: disable=no-member
                 )
 
-            return phonemes_str
-        finally:
-            self.libc.fclose(phonemes_file)
+        if not keep_language_flags:
+            # Remove language switching flags, e.g. (en)
+            phoneme_lines = [
+                Phonemizer.LANG_SWITCH_FLAG.sub("", line) for line in phoneme_lines
+            ]
+
+        if word_separator != " ":
+            # Split/re-join words
+            for line_idx in range(len(phoneme_lines)):
+                phoneme_lines[line_idx] = word_separator.join(
+                    phoneme_lines[line_idx].split()
+                )
+
+        # Re-insert clause breakers
+        if missing_breakers:
+            # pylint: disable=consider-using-enumerate
+            for line_idx in range(len(phoneme_lines)):
+                if line_idx < len(missing_breakers):
+                    phoneme_lines[line_idx] += (
+                        punctuation_separator + missing_breakers[line_idx]
+                    )
+
+        phonemes_str = word_separator.join(line.strip() for line in phoneme_lines)
+
+        if no_stress:
+            # Remove primary/secondary stress markers
+            phonemes_str = Phonemizer.STRESS_PATTERN.sub("", phonemes_str)
+
+        # Clean up multiple phoneme separators
+        if phoneme_separator:
+            phonemes_str = re.sub(
+                "[" + re.escape(phoneme_separator) + "]+",
+                phoneme_separator,
+                phonemes_str,
+            )
+
+        return phonemes_str
 
     def _maybe_init(self):
-        if self.libc and self.lib_espeak:
+        if self.lib_espeak:
             # Already initialized
             return
-
-        self.libc = ctypes.cdll.LoadLibrary("libc.so.6")
-        self.libc.open_memstream.restype = ctypes.POINTER(ctypes.c_char)
 
         try:
             self.lib_espeak = ctypes.cdll.LoadLibrary("libespeak-ng.so")
